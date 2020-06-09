44f85e7cef21a08ab064cd4d6bac501a603a8b69
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2020-06-09 16:11:13 +0200

    SignalR

---
+++ src/Teldat.DotnetCore/Teldat.Messages.Api/Hubs/AlarmsHub.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Teldat.Messages.Api.Hubs
+{
+    public class AlarmsHub : Hub
+    {
+        public override Task OnConnectedAsync()
+        {
+            return base.OnConnectedAsync();
+        }
+
+        public async Task SendAlarm(string message)
+        {
+            await Clients.All.SendAsync("Alarm", message);
+        }
+
+        public async Task SendCommand(string content)
+        {
+            await Clients.Others.SendAsync("Command", content);
+        }
+
+        public async Task Ping()
+        {
+            await Clients.Caller.SendAsync("Pong");
+        }
+    }
+}
---
+++ src/Teldat.DotnetCore/Teldat.Messages.ReceiverConsoleClient/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace Teldat.Messages.ReceiverConsoleClient
+{
+    class Program
+    {
+        // dotnet add package Microsoft.AspNetCore.SignalR.Client
+
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello Signal-R Receiver!");
+
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.White;
+
+            const string url = "http://localhost:5000/signalr/alarms";
+
+            HubConnection connection = new HubConnectionBuilder()
+                .WithUrl(url)
+                .Build();
+
+            await connection.StartAsync();
+
+            Console.WriteLine("Connected.");
+
+            connection.On<string>("Alarm", message => Console.WriteLine($"Received {message}"));
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+
+            Console.ResetColor();
+        }
+    }
+}

c64d9742052aff17e36bc5aa039b39aa37d0e3cb
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2020-06-10 10:42:03 +0200

    Utworzenie kontekstu

---
+++ src/Teldat.DotnetCore/Teldat.Vehicles.Infrastructure/DbServices/DbVehicleService.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Teldat.Vehicles.Domain.IServices;
+using Teldat.Vehicles.Domain.Models;
+
+namespace Teldat.Vehicles.Infrastructure.DbServices
+{
+    public class DbVehicleService : IVehicleService
+    {
+        private readonly VehiclesContext context;
+
+        public async Task Add(Vehicle vehicle)
+        {
+            await context.Vehicles.AddAsync(vehicle);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<Vehicle>> Get()
+        {
+            return await context.Vehicles.ToListAsync();
+        }
+
+        public async Task<Vehicle> Get(int id)
+        {
+            return await context.Vehicles.FindAsync(id);
+        }
+
+        public async Task<Vehicle> Get(string vin)
+        {
+            return await context.Vehicles.SingleOrDefaultAsync(v => v.Vin == vin);
+        }
+
+        public async Task Remove(int id)
+        {
+            Vehicle vehicle = await Get(id);
+
+            context.Vehicles.Remove(vehicle);
+
+            await context.SaveChangesAsync();
+        }
+
+        public Task Update(Vehicle vehicle)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Teldat.DotnetCore/Teldat.Vehicles.Infrastructure/DbServices/VehiclesContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Teldat.Vehicles.Domain.Models;
+
+namespace Teldat.Vehicles.Infrastructure.DbServices
+{
+
+    // dotnet add package Microsoft.EntityFrameworkCore
+    public class VehiclesContext : DbContext
+    {
+        public VehiclesContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Vehicle> Vehicles { get; set; }
+        public DbSet<Civil> Civils { get; set; }
+        public DbSet<Soldier> Soldiers { get; set; }
+
+    }
+}

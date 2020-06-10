c1a365a4f551875bd48494f63859765b0c997e26
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2020-06-10 14:01:17 +0200

    Refaktoring

---
+++ src/Teldat.DotnetCore/Teldat.Vehicles.Api/EFCoreExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Teldat.Vehicles.Api
+{
+    public static class EFCoreExtensions
+    {
+        public static IHost MigrateDbContext<TContext>(this IHost host)
+            where TContext : DbContext
+        {
+            var scope = host.Services.CreateScope();
+            var context = scope.ServiceProvider.GetService<TContext>();
+
+            context.Database.EnsureCreated();
+            // await context.Database.Migrate();
+
+            return host;
+        }
+
+    }
+}
--- src/Teldat.DotnetCore/Teldat.Vehicles.Api/Program.cs
+++ src/Teldat.DotnetCore/Teldat.Vehicles.Api/Program.cs
@@ -3,30 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Teldat.Vehicles.Infrastructure.DbServices;
 
 namespace Teldat.Vehicles.Api
 {
-    public static class EFCoreExtensions
-    {
-        public static IHost MigrateDbContext<TContext>(this IHost host)
-            where TContext : DbContext
-        {
-            var scope = host.Services.CreateScope();
-            var context = scope.ServiceProvider.GetService<TContext>();
-
-            context.Database.EnsureCreated();
-            // await context.Database.Migrate();
-
-            return host;
-        }
-
-    }
 
     public class Program
     {
